98294464df690aee9f890d772c5a06d1306effd4
Gerry <gerry@Gerrys-MacBook-Pro.local>
2019-06-10 16:23:11 -0400

    adding ansible Dockerfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "num_of_servers" {
-  default = 1
+variable "num_of_resource_servers" {
+  default = 2
 }
 
 resource "aws_instance" "captain" {
   ami           = "ami-01d9d5f6cecc31f85"
   instance_type = "t2.micro"
-  count         = "${var.num_of_servers}"
   key_name      = "grlaracuente-IAM"
 
-    user_data = <<EOF
-    
-    EOF
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -      
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+              sudo docker pull ansible/ansible:default
+              EOF
 
-    inline = [
-      "touch /gerry",
-      "mkdir /g3",
-    ]
-  }
-
-  tags {
+  tags = {
     Name = "captain"
   }
 }
+
+resource "aws_instance" "resource_server" {
+  ami           = "ami-01d9d5f6cecc31f85"
+  instance_type = "t2.micro"
+  count         = var.num_of_resource_servers
+  key_name      = "grlaracuente-IAM"
+
+  tags = {
+    Name = "resource_server"
+  }
+}
+
+output "captain_public_ip" {
+  value = ["${aws_instance.captain.public_ip}"]
+}
+
+output "captain_private_ip" {
+  value = ["${aws_instance.captain.private_ip}"]
+}
+
+output "resource_server_public_ips" {
+  value = ["${aws_instance.resource_server.*.public_ip}"]
+}
+
+output "resource_server_private_ips" {
+  value = ["${aws_instance.resource_server.*.private_ip}"]
+}
+
