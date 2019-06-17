1727d4eeaeea3422328cee3ef4b89cb010048205
Gerry <gerry@Gerrys-MacBook-Pro.local>
2019-06-17 15:45:10 -0400

    mostly ansible playbook updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "num_of_resource_servers" {
-  default = 2
+variable "num_of_resource_worker_servers" {
+  default = 6
 }
 
 resource "aws_instance" "captain" {
@@ -30,14 +30,37 @@ resource "aws_instance" "captain" {
   }
 }
 
-resource "aws_instance" "resource_server" {
+resource "aws_instance" "resource_server_master" {
   ami           = "ami-01d9d5f6cecc31f85"
-  instance_type = "t2.medium"
-  count         = var.num_of_resource_servers
+  instance_type = "t2.xlarge"
   key_name      = "grlaracuente-IAM"
 
   user_data = <<-EOF
               #!/bin/bash
+              #THIS SHOULD ALL REALLY HAPPEN IN ANSIBLE....SERVER SHOULD JUST NEED TO BE RUNNING UBUNTU
+              sudo apt-get update
+              sudo apt-get install -y python
+              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -      
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+              EOF
+
+  tags = {
+    Name = "resource_server"
+  }
+}
+
+resource "aws_instance" "resource_server_worker" {
+  ami           = "ami-01d9d5f6cecc31f85"
+  instance_type = "t2.micro"
+  count         = var.num_of_resource_worker_servers
+  key_name      = "grlaracuente-IAM"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              #THIS SHOULD ALL REALLY HAPPEN IN ANSIBLE....SERVER SHOULD JUST NEED TO BE RUNNING UBUNTU
               sudo apt-get update
               sudo apt-get install -y python
               sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -56,8 +79,12 @@ output "captain_public_ip" {
   value = ["${aws_instance.captain.public_ip}"]
 }
 
-output "resource_server_public_ips" {
-  value = ["${aws_instance.resource_server.*.public_ip}"]
+output "resource_server_master_public_ips" {
+  value = ["${aws_instance.resource_server_master.public_ip}"]
+}
+
+output "resource_server_worker_public_ips" {
+  value = ["${aws_instance.resource_server_worker.*.public_ip}"]
 }
 
 #output "resource_server_private_ips" {
