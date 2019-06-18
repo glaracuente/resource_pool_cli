639ddf0de0cb37a524efcd375c99690e9fc13a24
Gerry <gerry@Gerrys-MBP.fios-router.home>
2019-06-18 19:43:54 -0400

    moving some logic from terra to ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 variable "num_of_resource_worker_servers" {
-  default = 6
+  default = 10
 }
 
 resource "aws_instance" "captain" {
@@ -32,23 +32,11 @@ resource "aws_instance" "captain" {
 
 resource "aws_instance" "resource_server_master" {
   ami           = "ami-01d9d5f6cecc31f85"
-  instance_type = "t2.xlarge"
+  instance_type = "t2.medium"
   key_name      = "grlaracuente-IAM"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              #THIS SHOULD ALL REALLY HAPPEN IN ANSIBLE....SERVER SHOULD JUST NEED TO BE RUNNING UBUNTU
-              sudo apt-get update
-              sudo apt-get install -y python
-              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -      
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              sudo apt-get update
-              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-              EOF
-
   tags = {
-    Name = "resource_server"
+    Name = "resource_server_master"
   }
 }
 
@@ -58,20 +46,8 @@ resource "aws_instance" "resource_server_worker" {
   count         = var.num_of_resource_worker_servers
   key_name      = "grlaracuente-IAM"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              #THIS SHOULD ALL REALLY HAPPEN IN ANSIBLE....SERVER SHOULD JUST NEED TO BE RUNNING UBUNTU
-              sudo apt-get update
-              sudo apt-get install -y python
-              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -      
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              sudo apt-get update
-              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-              EOF
-
   tags = {
-    Name = "resource_server"
+    Name = "resource_server_worker"
   }
 }
 
@@ -86,8 +62,4 @@ output "resource_server_master_public_ips" {
 output "resource_server_worker_public_ips" {
   value = ["${aws_instance.resource_server_worker.*.public_ip}"]
 }
-
-#output "resource_server_private_ips" {
-#  value = ["${aws_instance.resource_server.*.private_ip_address}"]
-#}
 
