106ce2054bd7a9997f09607d6dbc9d2b5e616353
Gerry <gerry@Gerrys-MBP.fios-router.home>
2019-06-27 20:57:24 -0400

    cleaning up bash scripts

--- user_facing/resource_pool.sh
+++ user_facing/resource_pool.sh
@@ -6,9 +6,6 @@ if [[ $# -eq 0 ]] ; then
     RESOURCE_CLI_ARGS="-h"
 fi
 
-VERSION="develop"
+DIR_ANSIBLE="/etc/resource_pool/ansible"
 
-DIR_RESOURCE_POOL="/etc/resource_pool"
-DIR_ANSIBLE="${DIR_RESOURCE_POOL}/ansible"
-
-docker run -it -v ${DIR_ANSIBLE}:/etc/ansible -v ${DIR_ANSIBLE}/keys/:/root/.ssh/ glaracuente/resource_pool:${VERSION} ${RESOURCE_CLI_ARGS}
+docker run -it -v ${DIR_ANSIBLE}:/etc/ansible -v ${DIR_ANSIBLE}/keys/:/root/.ssh/ glaracuente/resource_pool:latest ${RESOURCE_CLI_ARGS}
